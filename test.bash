80220e624bee33eccb86242cba5ab09ed9647ffe
Takeru Yoshida <s22C1751jv@s.chibakoudai.jp>
2024-11-07 08:47:38 +0900

    update

--- test.bash
+++ test.bash
@@ -1,14 +1,16 @@
 #!/bin/bash
-
+# SPDX-FileCopyrightText: 2024 Takeru Yoshida
 ng () {
 	echo ${1}行目が違うよ
 	res=1
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK 
  exit $res
 
 
